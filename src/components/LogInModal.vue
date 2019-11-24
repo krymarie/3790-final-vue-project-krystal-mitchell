42772b86e3063fad6afd5aac8df0548a8d91e22e
Krystal Mitchell <10262399@my.uvu.edu>
2019-11-23 23:22:42 -0700

    all sorts of fun transitions, login modal

--- src/components/LogInModal.vue
+++ src/components/LogInModal.vue
@@ -1,50 +1,165 @@
+<style>
+html {
+  height: 100%;
+  background: #FFF;
+  color: #000;
+  font-size: 62.5%;
+}
+
+body {
+  min-height: 100%;
+  margin: 0;
+  display: grid;
+  place-items: center;
+  font-size: 1.4rem;
+}
+
+h1 {
+  font-size: 20px;
+}
+
+.button {
+  border: none;
+  color: #FFF;
+  background: #42b983;
+  appearance: none;
+  font: inherit;
+  font-size: 1.8rem;
+  padding: .5em 1em;
+  border-radius: .3em;
+  cursor: pointer;
+}
+
+.modal {
+  position: absolute;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  text-align: center;
+  width: 400px;
+  height: fit-content;
+//  max-width: 22em;
+  padding: 2rem;
+  border-radius: 1rem;
+  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.2);
+  background: #FFF;
+  z-index: 999;
+  transform: none;
+}
+.modal h1 {
+  margin: 0 0 1rem;
+}
+
+.modal-overlay {
+  content: '';
+  position: absolute;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 998;
+  background: #2c3e50;
+  opacity: 0.6;
+  cursor: pointer;
+}
+
+/* ---------------------------------- */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .4s linear;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.pop-enter-active,
+.pop-leave-active {
+  transition: transform 0.4s cubic-bezier(0.5, 0, 0.5, 1), opacity 0.4s linear;
+}
+
+.pop-enter,
+.pop-leave-to {
+  opacity: 0;
+  transform: scale(0.3) translateY(-50%);
+}
+
+</style>
+
+
 <template>
     <!-- CONTENT -->
     <v-content>
       <div id="app">
-  <v-app id="inspire">
-    <v-card color="grey lighten-4" flat>
-      <v-card-text>
-        <v-container fluid>
-          <v-layout row>
-            <v-flex xs8>
-              <v-text-field
+  <button @click="showModal = true" class="button">Log In</button>
+  <transition name="fade" appear>
+    <div class="modal-overlay" 
+         v-if="showModal" 
+         @click="showModal = false"></div>
+  </transition>
+  <transition name="pop" appear>
+    <div class="modal" 
+         role="dialog" 
+         v-if="showModal"
+         >
+      <h1>LogIn Transition</h1>
+      <v-flex xs8>
+        <v-text-field
                 name="input-10-1"
-                label="Enter your password"
-                hint="At least 8 characters"
-                v-model="password"
+                label="Enter your username"
+                hint="Favorite Car"
+                v-model="username"
                 min="8"
                 :append-icon="e1 ? 'visibility' : 'visibility_off'"
                 :append-icon-cb="() => (e1 = !e1)"
                 :type="e1 ? 'password' : 'text'"
                 counter
               ></v-text-field>
+        
+              <v-text-field
+                name="input-10-1"
+                label="Enter your password"
+                hint="At least 8 characters"
+                v-model="password"
+                min="8"
+                :append-icon="e2 ? 'visibility' : 'visibility_off'"
+                :append-icon-cb="() => (e2 = !e2)"
+                :type="e2 ? 'password' : 'text'"
+                counter
+              ></v-text-field>
             </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card-text>
-    </v-card>
-  </v-app>
+      <button @click="showModal = false" class="button">Log In</button>
+
+    </div>
+  </transition>
 </div>
     </v-content>
 </template>
 
 <script>
 new Vue({
-  el: "#app",
+
+  // Mount Vue instance to DOM with `el`
+  // https://vuejs.org/v2/api/#el
+  el: '#app',
+
+  // State with data
+  // http://vuejs.org/v2/guide/instance.html#Data-and-Methods
   data() {
     return {
-      e1: true
-      //set to variable check if password is set in store or default to empty string then
-      //update signedIn to True when logIn Happens correctly
-      //password: 'Password Visable'
-
-      //pull from store if signedIn = true
-      //leave blank if new to page or logedOut, look up auto populate for chrome pws storage
-      //for this class simply make it work always and link to CRM 
-      //add vue oath valuation...
-    };
-  }
+      showModal: false,
+      e1: false,
+      e2: true,
+      username: 'Mclaren',
+      //password: 'Password Visable',
+      //password: mclaren,
+    }
+  } 
 });
 
 </script>
