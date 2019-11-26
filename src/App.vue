9d49df9eca1c599a7f2d875d9bac21940c196104
Krystal Mitchell <10262399@my.uvu.edu>
2019-11-26 16:22:11 -0700

    something else ti figure out with tags are missing

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,48 @@
 <template>
+<!-- Broke out the top and side nav wrong my errors aren't css but vutify
+possibly combine them just to get it built then work on breaking it up again -->
   <v-app light="light">
  
     
-    <!-- v-app and app="app" don't need both in vuetify-->
-<v-navigation-drawer v-model="drawer" app="app" stateless="stateless" floating="floating" width="220">
+    <!-- v-navigation drawer-->
+    <v-navigation-drawer v-model="drawer" app="app" stateless="stateless" floating="floating" width="220">
       <v-toolbar class="blue darken-3">
         <v-list>
           <v-list-tile @click="">
             <v-list-tile-content><v-list-tile-title.title class=v-icon.mr-2 home>FLC Account Manager</v-list-tile-title.title>
-<v-icon>mdi-mouse</v-icon>
+              <v-icon>mdi-mouse</v-icon>
             </v-list-tile-content>
           </v-list-tile>
         </v-list>
-      </v-toolbar><v-list>
-      <v-list-tile v-for="(title, icon) in mainMenu" @click=""><v-list-tile-action>
-<v-icon> {{ icon }}</v-icon>
-</v-list-tile-action>
-<v-list-tile-content>
-<v-list-tile-title> {{ title }}</v-list-tile-title>
-</v-list-tile-content>  
-      </v-list-tile></v-list>
+      </v-toolbar>
+      <v-list>
+        <v-list-tile v-for="(title, icon) in mainMenu" @click="">
+          <v-list-tile-action>
+            <v-icon> {{ icon }}</v-icon>
+          </v-list-tile-action>
+      
+          <v-list-tile-content>
+            <v-list-tile-title> {{ title }}</v-list-tile-title>
+          </v-list-tile-content>  
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
+
+    <!-- Toolbar -->
     <v-toolbar class="blue darken-4" app="app">
+      
       <v-toolbar-side-icon @click.stop="clickToggleDrawer"></v-toolbar-side-icon>
+      
       <v-spacer></v-spacer>
+      
       <v-btn icon="icon">
         <v-icon>search</v-icon>
       </v-btn>
+      
       <v-btn icon="icon">
         <v-icon>email</v-icon>
       </v-btn>
+      
       <v-menu offset-y="offset-y">
         <v-btn flat="flat" slot="activator" small="small">FLC Account Manager
           <v-icon>keyboard_arrow_down</v-icon>
@@ -45,7 +58,9 @@
           </v-list-tile>
         </v-list>
       </v-menu>
-      <v-avatar class="mr-2" size="36"><img src="https://www.uvu.edu/dgm/web-design-dev/images/interaction/quote-krystal-mitchell.png"/></v-avatar>
+
+      <v-avatar class="mr-2" size="36"><img src="https://avatars3.githubusercontent.com/u/4356894?s=400&v=4"/></v-avatar>
+
     </v-toolbar>
 <!-- Import CRM Content Here -->
     <v-content> 
